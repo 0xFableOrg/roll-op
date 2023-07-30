e3ba3ee2d0dc047b3ff1609134f19609f1fcdc18
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-07-30 02:12:32 +0200

    roll.py

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Under development!
 
+This work is support by an
+[Optimism governance grant](https://app.charmverse.io/op-grants/proposals?id=a6e6bfb8-75bd-41bd-acb1-618c3c62e667).
+
 simple-op-stack rollup is open-source package that makes it trivial for any
 developer to spin up an OP stack rollup, both for development and production
 use.
@@ -14,3 +17,25 @@ Additionally, the package supports spinning EIP-4337 account abstraction
 infrastructure (a bundler and a paymaster) and helps you configure them so that
 you can automatically subsidize gas for transactions that match certain criteria
 (e.g. transactions to specific whitelisted contracts).
+
+## Prerequisites
+
+- Python 3 (to run the `roll.py` script)
+
+The following dependencies will be checked by `roll.py`:
+
+- `make`
+- Git
+- Go 1.19
+
+`roll.py` will check the following dependencies and install them for you if needed (the script will
+always for your permission before installing anything outside the current directory):
+
+- Node.js 16.x
+- Yarn (`npm install -g yarn` — the old one, not yarn v3 aka yarn berry)
+
+## Plans
+
+- See [here](https://app.charmverse.io/op-grants/proposals?id=a6e6bfb8-75bd-41bd-acb1-618c3c62e667)
+  for the initial description of the project including some milestones.
+- See [this document](https://hackmd.io/@vitalizing/SJXw9Wbih) for a more thoughtful architecture breakdown.
---
+++ roll.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import shutil
+import libroll as lib
+
+####################################################################################################
+# VARIABLES
+
+# Whether we must instruct to use the proper Node version via NVM or not.
+must_nvm_use = False
+
+####################################################################################################
+# CONSTANTS
+
+# Short name of the node version to use, for use in logs.
+NODE_VNAME = "v16"
+
+# Full node of the node version required by the optimism repository.
+NODE_VERSION = "v16.16.0"
+
+# Minimum Go version required by the optimism repository.
+GO_VERSION = "1.19"
+
+# Not a strict dependency, just a recent NVM version to install if needed.
+NVM_VERSION = "0.39.4"
+
+####################################################################################################
+# ARGUMENT PARSING
+
+# Store the parsed arguments here.
+global args
+
+parser = argparse.ArgumentParser(
+    description="Helps you spin up an op-stack rollup.")
+
+subparsers = parser.add_subparsers(
+    title="commands",
+    dest="command",
+    metavar="<command>")
+
+subparsers.add_parser(
+    "setup",
+    help="installs prerequisites and builds the optimism repository")
+
+parser.add_argument(
+    "--no-ansi-esc",
+    help="disable ANSI escape codes for terminal manipulation",
+    default=True,
+    dest="use_ansi_esc",
+    action="store_false")
+
+
+####################################################################################################
+# UTILITY
+
+def run_with_node(descr: str, command: str | list[str], **kwargs) -> str:
+    """
+    Just like :py:func:`run`, but prepends the command with an ``nvm use`` statement if necessary.
+    """
+    if must_nvm_use:
+        return lib.run(descr, f"source ~/.nvm/nvm.sh; nvm use {NODE_VERSION}; {command}", **kwargs)
+    else:
+        return lib.run(descr, command, **kwargs)
+
+
+####################################################################################################
+
+def check_prerequisites():
+    """
+    Check basic prerequisites for running this script, and print warnings for potential source of
+    troubles.
+    """
+
+    if shutil.which("make") is None:
+        raise Exception(
+            "Make is not installed. Please install it from your package manager." +
+            "e.g. `brew install make` or `sudo apt install build-essentials`")
+
+    if shutil.which("git") is None:
+        raise Exception(
+            "git is not installed. Please install it from your package manager." +
+            "e.g. `brew install gi` or `sudo apt install git`")
+
+    if shutil.which("go") is None:
+        raise Exception(
+            f"go is not installed. Please install Go **version {GO_VERSION}** or higher.")
+    elif lib.run("get go version", "go version") < GO_VERSION:
+        raise Exception(
+            f"Go version is too low. Please update to Go **version {GO_VERSION}** or higher."
+            + "Go is backwards compatible, so your old project will continue to build.")
+
+    if args.use_ansi_esc and not lib.is_well_known_term():
+        print(
+            "\nWARNING: Your terminal is weird."
+            + "This may cause it to not handle ANSI escape codes well."
+            + "You can disable them with TODO\n")
+
+
+####################################################################################################
+# SETUP
+
+def setup():
+    install_correct_node()
+    install_yarn()
+    setup_optimism_repo()
+
+
+# --------------------------------------------------------------------------------------------------
+
+def install_correct_node():
+    # Check if Node is installed and is the correct version.
+    if shutil.which("node") is not None:
+        if lib.run("get node version", "node --version") == NVM_VERSION:
+            return
+
+    # Either Node is not installed, or the current version is not v16.
+    # global must_nvm_use
+    global must_nvm_use
+    must_nvm_use = True
+
+    def nvm_install_node():
+        lib.run(f"install node {NODE_VNAME}", f"source ~/.nvm/nvm.sh; nvm install {NODE_VERSION}")
+
+    if os.path.isfile(os.path.expanduser("~/.nvm/nvm.sh")):
+        # We have NVM, try using required version or installing it.
+        try:
+            lib.run(f"use node {NODE_VNAME}", f"source ~/.nvm/nvm.sh; nvm use {NODE_VERSION}")
+        except Exception as err:
+            if lib.ask_yes_no(f"Node {NODE_VNAME} ({NODE_VERSION}) is required. NVM is installed. "
+                              f"Install with NVM?"):
+                nvm_install_node()
+            else:
+                raise Exception(f"Node {NODE_VNAME} ({NODE_VERSION}) is required.")
+    else:
+        # Install NVM + Node.
+        nvm_url = f"https://raw.githubusercontent.com/nvm-sh/nvm/{NVM_VERSION}/install.sh"
+        if lib.ask_yes_no(f"Node {NODE_VNAME} ({NODE_VERSION}) is required. Install NVM + Node?"):
+            lib.run("install nvm", f"curl -o- {nvm_url} | bash")
+            nvm_install_node()
+        else:
+            raise Exception(f"Node {NODE_VNAME} ({NODE_VERSION}) is required.")
+
+
+# --------------------------------------------------------------------------------------------------
+
+def install_yarn():
+    if shutil.which("yarn") is not None:
+        return
+
+    if lib.ask_yes_no("Yarn is required. Install?"):
+        run_with_node("install yarn", "npm install -g yarn")
+    else:
+        raise Exception("Yarn is required.")
+
+
+# --------------------------------------------------------------------------------------------------
+
+def setup_optimism_repo():
+    github_url = "git@github.com:ethereum-optimism/optimism.git"
+    git_tag = "op-node/v1.1.1"
+
+    if os.path.isfile("optimism"):
+        raise Exception("Error: 'optimism' exists as a file and not a directory.")
+    elif not os.path.exists("optimism"):
+        descr = "clone the optimism repository"
+        lib.run(descr, f"git clone {github_url}")
+        print(f"Succeeded: {descr}")
+
+    lib.run("checkout stable version", f"git checkout --detach {git_tag}", cwd="optimism")
+
+    print("Starting to build the optimism repository. This may take a while...\n")
+    # TODO screen position moves, so this only clears one screen worth of output
+    if args.use_ansi_esc: lib.term_save_cursor()
+    run_with_node("build optimism", "make build", cwd="optimism", forward_output=True)
+    if args.use_ansi_esc: lib.term_clear_from_saved()
+    # TODO tee the outputs to a log file
+    print("Successfully built the optimism repository.")
+
+
+####################################################################################################
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    try:
+        if args.command is None:
+            parser.print_help()
+            exit()
+
+        check_prerequisites()
+        if args.command == "setup":
+            setup()
+
+        print("Done.")
+    except Exception as e:
+        print(f"Aborted with error: {e}")
+
+####################################################################################################
