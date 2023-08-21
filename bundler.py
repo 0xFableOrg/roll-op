c8276f45ac62bbb4b6f89d779c1dc1e840c0ec40
eerkaijun <kaijuneer@gmail.com>
2023-08-21 16:45:57 +0800

    use existing helper functions

--- bundler.py
+++ bundler.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import shutil
 import libroll as lib
+import term 
+import deps
 
 ####################################################################################################
 # VARIABLES
@@ -43,51 +45,6 @@ parser.add_argument(
     action="store_false")
 
 ####################################################################################################
-# UTILITY
-
-def run_with_node(descr: str, command: str | list[str], **kwargs) -> str:
-    """
-    Just like :py:func:`run`, but prepends the command with an ``nvm use`` statement if necessary.
-    """
-    if must_nvm_use:
-        return lib.run(descr, f"source ~/.nvm/nvm.sh; nvm use {NODE_VERSION}; {command}", **kwargs)
-    else:
-        return lib.run(descr, command, **kwargs)
-
-####################################################################################################
-
-def check_prerequisites():
-    """
-    Check basic prerequisites for running this script, and print warnings for potential source of
-    troubles.
-    """
-
-    if shutil.which("make") is None:
-        raise Exception(
-            "Make is not installed. Please install it from your package manager." +
-            "e.g. `brew install make` or `sudo apt install build-essentials`")
-
-    if shutil.which("git") is None:
-        raise Exception(
-            "git is not installed. Please install it from your package manager." +
-            "e.g. `brew install gi` or `sudo apt install git`")
-
-    if shutil.which("go") is None:
-        raise Exception(
-            f"go is not installed. Please install Go **version {GO_VERSION}** or higher.")
-    elif lib.run("get go version", "go version") < GO_VERSION:
-        raise Exception(
-            f"Go version is too low. Please update to Go **version {GO_VERSION}** or higher."
-            + "Go is backwards compatible, so your old project will continue to build.")
-
-    if args.use_ansi_esc and not lib.is_well_known_term():
-        print(
-            "\nWARNING: Your terminal is weird."
-            + "This may cause it to not handle ANSI escape codes well."
-            + "You can disable them with TODO\n")
-
-
-####################################################################################################
 # SETUP
 
 def setup():
@@ -108,12 +65,12 @@ def setup_4337_contracts():
 
     # If contracts have not been previously deployed
     if not os.path.exists("account-abstraction/deployments/opstack"):
-        run_with_node("install account abstraction dependencies", "yarn install", cwd="account-abstraction", forward_output=True)
+        lib.run("install account abstraction dependencies", command=deps.cmd_with_node("yarn install"), cwd="account-abstraction")
         # we need to configure the network to point to local network
         # we also need to set private key for deployment
         priv_key = input(f"Enter private key that you would like to deploy contracts with: ")
         lib.run("set private key", f"echo PRIVATE_KEY={priv_key} > account-abstraction/.env")
-        run_with_node("deploy contracts", "yarn deploy --network opstack", cwd="account-abstraction", forward_output=True)
+        lib.run("deploy contracts", command=deps.cmd_with_node("yarn deploy --network opstack"), cwd="account-abstraction")
         print(f"Account abstraction contracts successfully deployed.")
     else:
         print(f"Account abstraction contracts already deployed.")
@@ -146,7 +103,8 @@ if __name__ == "__main__":
             parser.print_help()
             exit()
 
-        check_prerequisites()
+        deps.check_basic_prerequisites()
+        deps.check_go()
         if args.command == "setup":
             setup()
 
