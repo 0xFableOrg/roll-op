cb570aca203f35ca4ace361fdbd15e7d3afef9db
eerkaijun <kaijuneer@gmail.com>
2023-09-28 20:45:20 +0100

    fix go path

--- deps.py
+++ deps.py
@@ -216,8 +216,8 @@ def go_path_setup():
         os.environ.pop("GOROOT", None)  # works if GOROOT isn't set
 
     # make sure that GOPATH is set in PATH (this is needed for 4337 bundler)
-    gopath = lib.run("get GOPATH", "go env GOPATH")
-    lib.prepend_to_path(gopath)
+    gopath = lib.run("get GOPATH", "go env GOPATH").strip()
+    lib.prepend_to_path(gopath + "/bin")
 
 
 ####################################################################################################
