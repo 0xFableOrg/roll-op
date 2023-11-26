f5e81b1dcccd9b97cab999889dd6a94e24cbb8cb
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-11-27 00:21:01 +0100

    add deployer_account config matching deployer_key

--- config.py
+++ config.py
@@ -67,6 +67,11 @@ class Config:
         # ==========================================================================================
         # Private Keys
 
+        self.contract_deployer_account = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+        """
+        Account used to deploy contracts to L1.
+        """
+
         self.contract_deployer_key = (
             "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80")
         """
