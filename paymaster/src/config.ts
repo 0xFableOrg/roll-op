5622efe585c1db54de9bc33b4506b7f751c8af29
eerkaijun <kaijuneer@gmail.com>
2023-12-14 22:42:44 +0000

    config file to define whitelisted address

---
+++ paymaster/src/config.ts
@@ -0,0 +1,7 @@
+// If set to true, all transactions will be sponsored
+export const whitelistAll: Boolean = true;
+
+// Whitelisted addresses for paymaster to sponsor transactions
+export const whitelistedAddresses: string[] = [
+    // add addresses here
+];
