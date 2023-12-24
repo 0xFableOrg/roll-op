b54b35c95327e307c6722441550676277a2814b5
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-12-24 16:02:54 +0100

    separate out L2 engine configuration from the Config class

--- config/__init__.py
+++ config/__init__.py
@@ -6,6 +6,7 @@ from .devnet_l1 import DevnetL1Config
 from .explorer import ExplorerConfig
 from .governance import GovernanceConfig
 from .l1_contracts_deploy import L1ContractsDeployConfig
+from .l2_engine import L2EngineConfig
 from .network import NetworkConfig
 from .paths import PathsConfig
 
@@ -78,6 +79,7 @@ class Config(
     DevnetL1Config,
     L1ContractsDeployConfig,
     NetworkConfig,
+    L2EngineConfig,
     ExplorerConfig
 ):
 
@@ -107,80 +109,6 @@ class Config(
         # Mixin Constructors
 
         super().__init__()
-
-        # ==========================================================================================
-        # L2 Execution Engine Configuration
-
-        # See also the properties starting with `l2_engine` below which are paths derived from
-        # :py:attribute:`l2_engine_data_dir`.
-
-        self.l2_engine_verbosity = 3
-        """Geth verbosity level (from 0 to 5, see op-geth --help)."""
-
-        self.l2_engine_p2p_port = 30313
-        """Port to use for the p2p server of the L2 engine (30313 by default)."""
-
-        self.l2_engine_rpc_listen_addr = "0.0.0.0"
-        """Address the L2 engine http RPC server should bind to ("0.0.0.0" by default)."""
-
-        self.l2_engine_rpc_listen_port = 9545
-        """Port to use for the L2 engine http JSON-RPC server."""
-
-        self.l2_engine_rpc_ws_listen_addr = "0.0.0.0"
-        """Address the L2 engine WebSocket RPC server should bind to ("0.0.0.0" by default)."""
-
-        self.l2_engine_rpc_ws_listen_port = 9546
-        """Port to use for the WebSocket JSON_RPC server."""
-
-        self.l2_engine_authrpc_listen_addr = "0.0.0.0"
-        """Address the L2 engine authRPC server should bind to ("0.0.0.0" by default)."""
-
-        self.l2_engine_authrpc_listen_port = 9551
-        """Port to use for the L2 engine authRPC server (9551 by default)."""
-
-        self.l2_engine_history_transactions = 2350000
-        """
-        Number of recent blocks to maintain transactions index for (default = about one
-        year (geth default), 0 = entire chain)
-        
-        This is the `--txlookuplimit` option in geth <= 1.12 and `--history.transactions` in geth >=
-        1.13.
-        """
-
-        self.l2_engine_disable_tx_gossip = True
-        """
-        Whether to disable transaction pool gossiping (True by default).
-        
-        In a system with a single sequencer, publicizing the mempool holds very little advantage:
-        it can cause spam by MEV searchers trying to frontrun or backrun transactions.
-        On the flip side, if the node crashes, gossiping can help refill the sequencer's mempool.
-        
-        I believe it's possible to set this to False (enable gossip) but restrict the peers in
-        another way, such that "centralized redundancy" (multiple nodes ran by the same entity)
-        can be achieved.
-        
-        This is currently pretty irrelevant, because we hardcode the --maxpeers=0 and --nodiscover
-        flags.
-        """
-
-        # === Metrics ===
-
-        self.l2_engine_metrics = False
-        """
-        Whether to record metrics in the L2 engine (False by default).
-        """
-
-        self.l2_engine_metrics_listen_port = 8060
-        """
-        Port to the L2 engine metrics server should bind to (8060 by default).
-        Ignored if :py:attribute:`node_metrics` is False.
-        """
-
-        self.l2_engine_metrics_listen_addr = "0.0.0.0"
-        """
-        Address the L2 engine metrics server should bind to ("0.0.0.0" by default).
-        Ignored if :py:attribute:`node_metrics` is False.
-        """
 
         # ==========================================================================================
         # Node Configuration
