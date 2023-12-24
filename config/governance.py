2e0b4e40d4d1bfcfe78bb564e014dd501ec05dd5
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-12-24 02:30:03 +0100

    separate out governance configuration from the Config class

---
+++ config/governance.py
@@ -0,0 +1,24 @@
+class GovernanceConfig:
+    """
+    Configuration options related to governance.
+    """
+
+    # ==============================================================================================
+
+    def __init__(self):
+        self.enable_governance = False
+        """
+        Whether to deploy a governance token (False by default).
+        """
+
+        self.governance_token_symbol = "STONK"
+        """
+        If :py:attribute:`enable_governance` is True, the symbol of the governance token to deploy.
+        """
+
+        self.governance_token_name = "Simple Token Op-chain Network Koin"
+        """
+        If :py:attribute:`enable_governance` is True, the name of the governance token to deploy.
+        """
+
+    # ==============================================================================================
