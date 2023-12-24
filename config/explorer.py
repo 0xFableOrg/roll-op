a68c803c1bcc7d98573762d0a2345519e1782f93
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-12-24 15:58:16 +0100

    separate out explorer config from the Config class

---
+++ config/explorer.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+
+class ExplorerConfig(ABC):
+    """
+    Configuration options related to the block explorer.
+    """
+
+    # ==============================================================================================
+    # Required Properties
+
+    @property
+    @abstractmethod
+    def deployment_name(self):
+        pass
+
+    # ==============================================================================================
+
+    def __init__(self):
+        super().__init__()
+
+        self.chain_name = f"roll-op <{self.deployment_name}>"
+        """
+        Name of the chain, notably used in the block explorer. Defaults to "roll-op
+        <{deployment_name}>".
+        """
+
+        self.chain_short_name = "roll-op"
+        """
+        Short version of :py:attribute:`chain_name`, used in the block explorer.
+        Defaults to "roll-op".
+        """
+
+    # ==============================================================================================
