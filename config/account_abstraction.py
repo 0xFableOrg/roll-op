8bfe6a56d1854762bfb455263927ea7d6bb006a7
Nicolas "Norswap" Laurent <norswap@gmail.com>
2023-12-25 20:40:26 +0100

    move AA keys to KeysAccountsConfig

--- config/account_abstraction.py
+++ config/account_abstraction.py
@@ -8,24 +8,6 @@ class AccountAbstractionConfig:
     def __init__(self):
         super().__init__()
 
-        self.aa_deployer_key = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-        """
-        Private key to use for deploying 4337 contracts.
-        Uses the 0th "test junk" mnemonnic key by default.
-        """
-
-        self.bundler_key = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-        """
-        Private key to use for submitting bundled transactions.
-        Uses the 0th "test junk" mnemonnic key by default.
-        """
-
-        self.paymaster_key = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-        """
-        Private key to use as paymaster signer.
-        Uses the 0th "test junk" mnemonnic key by default.
-        """
-
         self.paymaster_validity = 300
         """
         Time validity (in seconds) for the sponsored transaction that is signed by paymaster.
@@ -38,5 +20,12 @@ class AccountAbstractionConfig:
         It's recommended not to mess with this, it's only in the config because it's written and
         read in different locations, so it helps to maintain a single source of truth.
         """
+
+    # ==============================================================================================
+
+    # Also needed to run the bundler and paymster:
+    # :py:attribute:`aa_deployer_key` in :py:class:`config.AccountAbstractionConfig`
+    # :py:attribute:`bundler_key` in :py:class:`config.AccountAbstractionConfig`
+    # :py:attribute:`paymaster_key` in :py:class:`config.AccountAbstractionConfig`
 
     # ==============================================================================================
